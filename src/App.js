d2d897d9a68c908762d594968674630cc57644be
Sona Sreedhar <sonasreedhar44@gmail.com>
2023-06-06 14:04:50 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import React from 'react';
 import Header from "./components/Header";
-import Home from "./components/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   
   return (
    <div className='App'>
      <Header />
-     <Home />
-    
    </div>
   );
     
